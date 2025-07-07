5df8ade0841f7d3d25bafc76c9f86516d8847dfb
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-07 15:12:46 +0200

    feat(function number random)

--- pari_e_dispari/script.js
+++ pari_e_dispari/script.js
@@ -4,4 +4,6 @@ const numeroUtente = prompt("scegli un numero del 1 a 5");
 
 //funzione per numero random
 
-
+function numeroRandom (min , max){
+    return Math.floor(Math.random() * (max-min+1) ) + min ; 
+}
