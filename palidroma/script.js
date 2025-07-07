b6e17ba77c7cb6c4808200760d58813714775c3d
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-07 14:17:40 +0200

    feat (check polidroma)

--- palidroma/script.js
+++ palidroma/script.js
@@ -1,3 +1,10 @@
  //richiesta di una parola al utente
  const parola = prompt ("inserisci una parola");
+
+//check polidroma
+ function palindroma (parola){
+    parolaAlContrario = parola.split ('') .join('').reverse;  
+    
+    return pareola ===parolaAlContrario;
+ }
  
