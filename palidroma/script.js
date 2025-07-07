294bfd31c87d6b6cdd392e0ba28591fcd9366063
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-07 14:34:10 +0200

    feat(verify)

--- palidroma/script.js
+++ palidroma/script.js
@@ -2,9 +2,17 @@
  const parola = prompt ("inserisci una parola");
 
 //check polidroma
- function palindroma (parola){
-    parolaAlContrario = parola.split ('') .join('').reverse;  
+ function palidroma (parola){
+    const parolaAlContrario = parola.split ('') .join ('') .reverse;  //reverse e necesaria??
     
-    return pareola ===parolaAlContrario;
+    return parola === parolaAlContrario;
+
+
  }
- 
+
+ //verifica
+if ( palidroma (parola)){
+    console.log ("la parola è polidroma!");
+}else{
+    console.log("la parola NON è polidroma!")
+}
