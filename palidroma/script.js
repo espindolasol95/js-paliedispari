e0818955239dc9ab58c4f83767bc0b84e53fbc8f
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-09 09:28:47 +0200

    tests(paridispari)

--- palidroma/script.js
+++ palidroma/script.js
@@ -1,15 +1,15 @@
  //richiesta di una parola al utente
- const parola = prompt ("inserisci una parola");
+ const parola = prompt("inserisci una parola");
 
 //check polidroma
- function palidroma (parola){
-    const parolaAlContrario = parola.split(''). reverse(). join('') . toLowerCase();
+ function palidroma(parola){
+    const parolaAlContrario = parola.split(''). reverse(). join('').toLowerCase();
     return parola === parolaAlContrario
  }
 
  //verifica
-if ( palidroma (parola)){
-    console.log ("la parola è palidroma!");
+if ( palidroma(parola)){
+    console.log("la parola è palidroma!");
 }else{
     console.log("la parola NON è palidroma!")
 }
